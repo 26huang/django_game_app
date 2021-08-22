781f7985ad0d63e5291265cdb3fce33ed2c3a97a
26huang <weishuoh@gmail.com>
2021-08-22 15:59:12 -0400

    added Game and Move to admin console and added __str__ to display useful info in admin console

--- tictactoe/apps/gameplay/models.py
+++ tictactoe/apps/gameplay/models.py
@@ -9,6 +9,9 @@ class Game(models.Model):
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F')
 
+    def __str__(self):
+        return '{0} vs {1}'.format(self.first_player, self.second_player)
+
 
 class Move(models.Model):
     x = models.IntegerField()
@@ -16,4 +19,7 @@ class Move(models.Model):
     comment = models.CharField(max_length=100, blank=True)
     by_first_player = models.BooleanField()
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'x: {0} y: {1}'.format(self.x, self.y)
 
