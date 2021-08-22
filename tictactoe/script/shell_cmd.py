ea7193469210f204998ad3a7693a471e9cdda12d
26huang <weishuoh@gmail.com>
2021-08-22 17:02:31 -0400

    added script with project shell commands

---
+++ tictactoe/script/shell_cmd.py
@@ -0,0 +1,29 @@
+# shell command examples after running
+# $python manage.py shell
+
+from apps.gameplay.models import Game, Move
+
+# view Game object and fields
+Game.objects.all()
+g = Game.objects.all(pk=1)
+g.last_active
+
+# change and save values
+g.status = 'S'
+g.save()
+
+# filter results
+Game.objects.filter(status='F')
+Game.objects.exclude(status='F')
+Game.objects.filter(second_player__username='wei')
+
+# insert new data
+m = Move(x=0, y=1, comment='GL!', by_first_player=True, game=g)
+m.save()
+
+# get game info
+m.game
+g.move_set
+g.move_set.all()
+g.move_set.count()
+g.move_set.exclude(comment='')
