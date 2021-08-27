836c368b250cf089a177cc692802df74486fe3a1
26huang <weishuoh@gmail.com>
2021-08-27 00:27:42 -0400

    updated global static and added title to template

--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('main/', include('apps.main.urls')),
+    path('admin', admin.site.urls),
+    path('', include('apps.main.urls')),
     path('player/', include('apps.player.urls')),
 ]
