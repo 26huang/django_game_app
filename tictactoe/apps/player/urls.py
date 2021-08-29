d06f4f3c0c2b80117fda2efb75a10be0f4f03197
26huang <weishuoh@gmail.com>
2021-08-29 01:47:39 -0400

    added login and logout page

--- tictactoe/apps/player/urls.py
+++ tictactoe/apps/player/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
+from django.contrib.auth.views import LoginView, LogoutView
 
-app_name = 'main'
+app_name = 'player'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/',
+         LoginView.as_view(template_name='player/login_form.html'),
+         name='player_login'),
+    path('logout/',
+         LogoutView.as_view(),
+         name='player_logout'),
     # url(r'^$', views.home, name='home'),
 ]
--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.main.urls', namespace='main')),
-    path('player/', include('apps.player.urls', namespace='player')),
+    path('', include('apps.main.urls')),
+    path('player/', include('apps.player.urls')),
 ]
